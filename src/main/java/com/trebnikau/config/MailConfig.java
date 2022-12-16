155c6562c6d8aee12ad5c582b1802f3d75a87850
Stanislau Trebnikau <stasonpokemon@icloud.com>
2022-12-16 23:40:30 +0300

    Return code to MailConfig

--- src/main/java/com/trebnikau/config/MailConfig.java
+++ src/main/java/com/trebnikau/config/MailConfig.java
@@ -11,5 +11,30 @@ import java.util.Properties;
 @Configuration
 public class MailConfig {
 
+    @Value("${spring.mail.host}")
+    private String host;
+    @Value("${spring.mail.username}")
+    private String username;
+    @Value("${spring.mail.password}")
+    private String password;
+    @Value("${spring.mail.port}")
+    private int port;
+    @Value("${spring.mail.protocol}")
+    private String protocol;
+    @Value("${mail.debug}")
+    private String debug;
 
+
+    @Bean
+    public JavaMailSender getJavaMailSender() {
+        JavaMailSenderImpl mailSender = new JavaMailSenderImpl();
+        mailSender.setHost(host);
+        mailSender.setPort(port);
+        mailSender.setUsername(username);
+        mailSender.setPassword(password);
+        Properties javaMailProperties = mailSender.getJavaMailProperties();
+        javaMailProperties.setProperty("mail.transport.protocol", protocol);
+        javaMailProperties.setProperty("mail.debug", debug);
+        return mailSender;
+    }
 }
